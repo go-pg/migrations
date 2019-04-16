76aeca8c76c97a8a755285722cf5ab1481d423e7
Ilya Kaznacheev <dreamworm@ya.ru>
2019-04-16 20:04:31 +0300

    Add init option description to the example

--- example/main.go
+++ example/main.go
@@ -10,6 +10,7 @@ import (
 )
 
 const usageText = `This program runs command on the db. Supported commands are:
+  - init - creates version info table in the database
   - up - runs all available migrations.
   - up [target] - runs available migrations up to the target one.
   - down - reverts last migration.
