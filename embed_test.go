816afdb59212333a3b59c483c50485596c8cdbe2
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2021-03-08 15:05:19 +0200

    Move filepath.Abs

--- embed_test.go
+++ embed_test.go
@@ -10,10 +10,11 @@ import (
 var embedSQLMigrations embed.FS
 
 func TestEmbedFS(t *testing.T) {
-	DefaultCollection.DisableSQLAutodiscover(true)
-	DefaultCollection.DiscoverSQLMigrationsFromFilesystem(http.FS(embedSQLMigrations), "/not-existing-dir")
-	DefaultCollection.DiscoverSQLMigrationsFromFilesystem(http.FS(embedSQLMigrations), "/testdata/sqlmigrations")
-	m := DefaultCollection.Migrations()
+	coll := NewCollection()
+	coll.DisableSQLAutodiscover(true)
+	coll.DiscoverSQLMigrationsFromFilesystem(http.FS(embedSQLMigrations), "not-existing-dir")
+	coll.DiscoverSQLMigrationsFromFilesystem(http.FS(embedSQLMigrations), "testdata/sqlmigrations")
+	m := coll.Migrations()
 	if len(m) != 1 {
 		t.Fatal("could not init migrations from filesystem")
 	}
